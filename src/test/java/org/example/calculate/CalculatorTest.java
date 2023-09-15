3f755fc7407c9622c5915bcb4bdf2cf27d6b66ff
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2023-09-15 18:34:12 +0900

    ﻿사칙연산 계산기 구현
    - 간단 정리 내용
    https://blog.naver.com/leticia_14/223211356331
    
    - 실습내용
    ﻿https://blog.naver.com/leticia_14/223211503360
    
    - 객체지향 패러다임을 적용해서 계산기 만들

--- src/test/java/org/example/calculate/CalculatorTest.java
+++ src/test/java/org/example/calculate/CalculatorTest.java
@@ -1,17 +1,13 @@
-package org.example;
+package org.example.calculate;
 
-import org.example.calculate.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
